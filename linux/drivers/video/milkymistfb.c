05648398d366125d2c529851eb5bf8f5b6bbbf0a
Takeshi MATSUYA <macchan@sfc.wide.ad.jp>
2009-09-11 23:09:00 +0900

    Added milkymist framebuffer driver (WIP)

--- linux/drivers/video/milkymistfb.c
+++ linux/drivers/video/milkymistfb.c
@@ -1,5 +1,6 @@
 /*
- *      linux/drivers/video/milkymistfb.c
+ *  linux/drivers/video/milkymistfb.c -- Milkymist frame buffer device
+ *
  */
 
 #include <linux/module.h>
@@ -8,139 +9,529 @@
 #include <linux/string.h>
 #include <linux/mm.h>
 #include <linux/slab.h>
+#include <linux/vmalloc.h>
 #include <linux/delay.h>
-#include <linux/init.h>
+#include <linux/interrupt.h>
+#include <linux/platform_device.h>
+
+#include <asm/uaccess.h>
 #include <linux/fb.h>
-//#include <video/milkymistfb.h>
+#include <linux/init.h>
 
-#define MILKYMIST_FBMEM_START	(0x43000000)
+    /*
+     *  RAM we reserve for the frame buffer. This defines the maximum screen
+     *  size
+     *
+     *  The default can be overridden if the driver is compiled as a module
+     */
 
-#define MMPTR(x)		(*((volatile unsigned int *)(x)))
-#define CSR_VGA_RESET		MMPTR(0x80003000)
+#define VIDEOMEMSIZE	(1*1024*1024)	/* 1 MB */
 
-#define VGA_RESET		(0x01)
+#define	MMPTR(x)		(*((volatile unsigned int *)(x)))
+#define	CSR_VGA_RESET		MMPTR(0x80003000)
 
-#define CSR_VGA_HRES		MMPTR(0x80003004)
-#define CSR_VGA_HSYNC_START	MMPTR(0x80003008)
-#define CSR_VGA_HSYNC_END	MMPTR(0x8000300C)
-#define CSR_VGA_HSCAN		MMPTR(0x80003010)
+#define	VGA_RESET		(0x01)
 
-#define CSR_VGA_VRES		MMPTR(0x80003014)
-#define CSR_VGA_VSYNC_START	MMPTR(0x80003018)
-#define CSR_VGA_VSYNC_END	MMPTR(0x8000301C)
-#define CSR_VGA_VSCAN		MMPTR(0x80003020)
+#define	CSR_VGA_HRES		MMPTR(0x80003004)
+#define	CSR_VGA_HSYNC_START	MMPTR(0x80003008)
+#define	CSR_VGA_HSYNC_END	MMPTR(0x8000300C)
+#define	CSR_VGA_HSCAN		MMPTR(0x80003010)
 
-#define CSR_VGA_BASEADDRESS	MMPTR(0x80003024)
-#define CSR_VGA_BASEADDRESS_ACT	MMPTR(0x80003028)
+#define	CSR_VGA_VRES		MMPTR(0x80003014)
+#define	CSR_VGA_VSYNC_START	MMPTR(0x80003018)
+#define	CSR_VGA_VSYNC_END	MMPTR(0x8000301C)
+#define	CSR_VGA_VSCAN		MMPTR(0x80003020)
 
-#define CSR_VGA_BURST_COUNT	MMPTR(0x8000302C)
+#define	CSR_VGA_BASEADDRESS	MMPTR(0x80003024)
+#define	CSR_VGA_BASEADDRESS_ACT	MMPTR(0x80003028)
+
+#define	CSR_VGA_BURST_COUNT	MMPTR(0x8000302C)
 
 
-/* bootinfo.h defines the machine type values, needed when checking */
-/* whether are really running on a milkymist, KM                       */
-//#include <asm/bootinfo.h>
+static void *videomemory;
+static u_long videomemorysize = VIDEOMEMSIZE;
+module_param(videomemorysize, ulong, 0);
 
-static struct fb_info fb_info;
-
-static struct fb_var_screeninfo milkymistfb_defined = {
+static struct fb_var_screeninfo milkymistfb_default __initdata = {
 	.xres =		640,
 	.yres =		480,
 	.xres_virtual =	640,
 	.yres_virtual =	480,
-	.bits_per_pixel =16,
-	.red =		{ 11, 5, 0 },
-	.green =	{  5, 6, 0 },
-	.blue =		{  0, 5, 0 },
-	.activate =	FB_ACTIVATE_NOW,
-	.height =	-1,
-	.width =	-1,
-	.vmode =	FB_VMODE_NONINTERLACED,
+	.bits_per_pixel = 16,
+	.red =		{11, 5, 0 },
+      	.green =	{ 5, 6, 0 },
+      	.blue =		{ 0, 5, 0 },
+      	.activate =	FB_ACTIVATE_NOW,
+      	.height =	-1,
+      	.width =	-1,
+      	.vmode =	FB_VMODE_NONINTERLACED,
 };
 
-static struct fb_fix_screeninfo milkymistfb_fix = {
-	.id =		"Milkymist",
+static struct fb_fix_screeninfo milkymistfb_fix __initdata = {
+	.id =		"MilkymistFB",
 	.type =		FB_TYPE_PACKED_PIXELS,
 	.visual =	FB_VISUAL_TRUECOLOR,
-	.smem_len =	(640*480*2),
+	.smem_len = 	(640*480*2),
 	.line_length =	480,
 	.accel =	FB_ACCEL_NONE,
 };
 
-/* Set the palette */
-static int milkymistfb_setcolreg(unsigned regno, unsigned red, unsigned green,
-			      unsigned blue, unsigned transp, struct fb_info *info)
-{
-	/* value to be written into the palette reg. */
-	unsigned long hw_colorvalue = 0;
+static int milkymistfb_enable __initdata = 0;	/* disabled by default */
+module_param(milkymistfb_enable, bool, 0);
 
-	red   >>= 8;    /* The cmap fields are 16 bits    */
-	green >>= 8;    /* wide, but the harware colormap */
-	blue  >>= 8;    /* registers are only 8 bits wide */
-
-	hw_colorvalue = (blue << 16) + (green << 8) + (red);
-
-	return 0;
-}
+static int milkymistfb_check_var(struct fb_var_screeninfo *var,
+			 struct fb_info *info);
+static int milkymistfb_set_par(struct fb_info *info);
+static int milkymistfb_setcolreg(u_int regno, u_int red, u_int green, u_int blue,
+			 u_int transp, struct fb_info *info);
+static int milkymistfb_pan_display(struct fb_var_screeninfo *var,
+			   struct fb_info *info);
+static int milkymistfb_mmap(struct fb_info *info,
+		    struct vm_area_struct *vma);
 
 static struct fb_ops milkymistfb_ops = {
-	.owner		= THIS_MODULE,
+	.fb_read        = fb_sys_read,
+	.fb_write       = fb_sys_write,
+	.fb_check_var	= milkymistfb_check_var,
+	.fb_set_par	= milkymistfb_set_par,
 	.fb_setcolreg	= milkymistfb_setcolreg,
-	.fb_fillrect	= cfb_fillrect,
-	.fb_copyarea	= cfb_copyarea,
-	.fb_imageblit	= cfb_imageblit,
+	.fb_pan_display	= milkymistfb_pan_display,
+	.fb_fillrect	= sys_fillrect,
+	.fb_copyarea	= sys_copyarea,
+	.fb_imageblit	= sys_imageblit,
+	.fb_mmap	= milkymistfb_mmap,
 };
 
-int __init milkymistfb_init(void)
+    /*
+     *  Internal routines
+     */
+
+static u_long get_line_length(int xres_virtual, int bpp)
 {
-	unsigned long fboff;
-	unsigned long fb_start;
-	int i;
+	u_long length;
 
-	if (fb_get_options("milkymistfb", NULL))
-		return -ENODEV;
+	length = xres_virtual * bpp;
+	length = (length + 31) & ~31;
+	length >>= 3;
+	return (length);
+}
 
-#if 0
-	/* Validate we're on the proper machine type */
-	if (mips_machtype != MACH_DS5000_XX) {
-		return -EINVAL;
+    /*
+     *  Setting the video mode has been split into two parts.
+     *  First part, xxxfb_check_var, must not write anything
+     *  to hardware, it should only verify and adjust var.
+     *  This means it doesn't alter par but it does use hardware
+     *  data from it to check this var. 
+     */
+
+static int milkymistfb_check_var(struct fb_var_screeninfo *var,
+			 struct fb_info *info)
+{
+	u_long line_length;
+
+	/*
+	 *  FB_VMODE_CONUPDATE and FB_VMODE_SMOOTH_XPAN are equal!
+	 *  as FB_VMODE_SMOOTH_XPAN is only used internally
+	 */
+
+	if (var->vmode & FB_VMODE_CONUPDATE) {
+		var->vmode |= FB_VMODE_YWRAP;
+		var->xoffset = info->var.xoffset;
+		var->yoffset = info->var.yoffset;
 	}
-#endif
 
-	printk(KERN_INFO "Milkymistfb: Milkymist system detected\n");
-	printk(KERN_INFO "Milkymistfb: initializing onboard framebuffer\n");
+	/*
+	 *  Some very basic checks
+	 */
+	if (!var->xres)
+		var->xres = 1;
+	if (!var->yres)
+		var->yres = 1;
+	if (var->xres > var->xres_virtual)
+		var->xres_virtual = var->xres;
+	if (var->yres > var->yres_virtual)
+		var->yres_virtual = var->yres;
+	if (var->bits_per_pixel <= 1)
+		var->bits_per_pixel = 1;
+	else if (var->bits_per_pixel <= 8)
+		var->bits_per_pixel = 8;
+	else if (var->bits_per_pixel <= 16)
+		var->bits_per_pixel = 16;
+	else if (var->bits_per_pixel <= 24)
+		var->bits_per_pixel = 24;
+	else if (var->bits_per_pixel <= 32)
+		var->bits_per_pixel = 32;
+	else
+		return -EINVAL;
 
-	/* Framebuffer display memory base address */
-	fb_start = MILKYMIST_FBMEM_START;
+	if (var->xres_virtual < var->xoffset + var->xres)
+		var->xres_virtual = var->xoffset + var->xres;
+	if (var->yres_virtual < var->yoffset + var->yres)
+		var->yres_virtual = var->yoffset + var->yres;
 
-	/* Clear screen */
-	for (fboff = fb_start; fboff < fb_start + 640*480*2; fboff+=2)
-		*(volatile unsigned short *)fboff = 0x000;
+	/*
+	 *  Memory limit
+	 */
+	line_length =
+	    get_line_length(var->xres_virtual, var->bits_per_pixel);
+	if (line_length * var->yres_virtual > videomemorysize)
+		return -ENOMEM;
 
-	milkymistfb_fix.smem_start = fb_start;
-	CSR_VGA_BASEADDRESS = fb_start;
-	
+	/*
+	 * Now that we checked it we alter var. The reason being is that the video
+	 * mode passed in might not work but slight changes to it might make it 
+	 * work. This way we let the user know what is acceptable.
+	 */
+	switch (var->bits_per_pixel) {
+	case 1:
+	case 8:
+		var->red.offset = 0;
+		var->red.length = 8;
+		var->green.offset = 0;
+		var->green.length = 8;
+		var->blue.offset = 0;
+		var->blue.length = 8;
+		var->transp.offset = 0;
+		var->transp.length = 0;
+		break;
+	case 16:		/* RGBA 5551 */
+		if (var->transp.length) {
+			var->red.offset = 0;
+			var->red.length = 5;
+			var->green.offset = 5;
+			var->green.length = 5;
+			var->blue.offset = 10;
+			var->blue.length = 5;
+			var->transp.offset = 15;
+			var->transp.length = 1;
+		} else {	/* RGB 565 */
+			var->red.offset = 0;
+			var->red.length = 5;
+			var->green.offset = 5;
+			var->green.length = 6;
+			var->blue.offset = 11;
+			var->blue.length = 5;
+			var->transp.offset = 0;
+			var->transp.length = 0;
+		}
+		break;
+	case 24:		/* RGB 888 */
+		var->red.offset = 0;
+		var->red.length = 8;
+		var->green.offset = 8;
+		var->green.length = 8;
+		var->blue.offset = 16;
+		var->blue.length = 8;
+		var->transp.offset = 0;
+		var->transp.length = 0;
+		break;
+	case 32:		/* RGBA 8888 */
+		var->red.offset = 0;
+		var->red.length = 8;
+		var->green.offset = 8;
+		var->green.length = 8;
+		var->blue.offset = 16;
+		var->blue.length = 8;
+		var->transp.offset = 24;
+		var->transp.length = 8;
+		break;
+	}
+	var->red.msb_right = 0;
+	var->green.msb_right = 0;
+	var->blue.msb_right = 0;
+	var->transp.msb_right = 0;
 
-	fb_info.fbops = &milkymistfb_ops;
-	fb_info.screen_base = (char *)milkymistfb_fix.smem_start;
-	fb_info.var = milkymistfb_defined;
-	fb_info.fix = milkymistfb_fix;
-	fb_info.flags = FBINFO_DEFAULT;
-
-	fb_alloc_cmap(&fb_info.cmap, 256, 0);
-
-	if (register_framebuffer(&fb_info) < 0)
-		return 1;
 	return 0;
 }
 
-static void __exit milkymistfb_exit(void)
+/* This routine actually sets the video mode. It's in here where we
+ * the hardware state info->par and fix which can be affected by the 
+ * change in par. For this driver it doesn't do much. 
+ */
+static int milkymistfb_set_par(struct fb_info *info)
 {
-	unregister_framebuffer(&fb_info);
+	info->fix.line_length = get_line_length(info->var.xres_virtual,
+						info->var.bits_per_pixel);
+	return 0;
 }
 
-#ifdef MODULE
-MODULE_LICENSE("GPL");
+    /*
+     *  Set a single color register. The values supplied are already
+     *  rounded down to the hardware's capabilities (according to the
+     *  entries in the var structure). Return != 0 for invalid regno.
+     */
+
+static int milkymistfb_setcolreg(u_int regno, u_int red, u_int green, u_int blue,
+			 u_int transp, struct fb_info *info)
+{
+	if (regno >= 256)	/* no. of hw registers */
+		return 1;
+	/*
+	 * Program hardware... do anything you want with transp
+	 */
+
+	/* grayscale works only partially under directcolor */
+	if (info->var.grayscale) {
+		/* grayscale = 0.30*R + 0.59*G + 0.11*B */
+		red = green = blue =
+		    (red * 77 + green * 151 + blue * 28) >> 8;
+	}
+
+	/* Directcolor:
+	 *   var->{color}.offset contains start of bitfield
+	 *   var->{color}.length contains length of bitfield
+	 *   {hardwarespecific} contains width of RAMDAC
+	 *   cmap[X] is programmed to (X << red.offset) | (X << green.offset) | (X << blue.offset)
+	 *   RAMDAC[X] is programmed to (red, green, blue)
+	 * 
+	 * Pseudocolor:
+	 *    uses offset = 0 && length = RAMDAC register width.
+	 *    var->{color}.offset is 0
+	 *    var->{color}.length contains widht of DAC
+	 *    cmap is not used
+	 *    RAMDAC[X] is programmed to (red, green, blue)
+	 * Truecolor:
+	 *    does not use DAC. Usually 3 are present.
+	 *    var->{color}.offset contains start of bitfield
+	 *    var->{color}.length contains length of bitfield
+	 *    cmap is programmed to (red << red.offset) | (green << green.offset) |
+	 *                      (blue << blue.offset) | (transp << transp.offset)
+	 *    RAMDAC does not exist
+	 */
+#define CNVT_TOHW(val,width) ((((val)<<(width))+0x7FFF-(val))>>16)
+	switch (info->fix.visual) {
+	case FB_VISUAL_TRUECOLOR:
+	case FB_VISUAL_PSEUDOCOLOR:
+		red = CNVT_TOHW(red, info->var.red.length);
+		green = CNVT_TOHW(green, info->var.green.length);
+		blue = CNVT_TOHW(blue, info->var.blue.length);
+		transp = CNVT_TOHW(transp, info->var.transp.length);
+		break;
+	case FB_VISUAL_DIRECTCOLOR:
+		red = CNVT_TOHW(red, 8);	/* expect 8 bit DAC */
+		green = CNVT_TOHW(green, 8);
+		blue = CNVT_TOHW(blue, 8);
+		/* hey, there is bug in transp handling... */
+		transp = CNVT_TOHW(transp, 8);
+		break;
+	}
+#undef CNVT_TOHW
+	/* Truecolor has hardware independent palette */
+	if (info->fix.visual == FB_VISUAL_TRUECOLOR) {
+		u32 v;
+
+		if (regno >= 16)
+			return 1;
+
+		v = (red << info->var.red.offset) |
+		    (green << info->var.green.offset) |
+		    (blue << info->var.blue.offset) |
+		    (transp << info->var.transp.offset);
+		switch (info->var.bits_per_pixel) {
+		case 8:
+			break;
+		case 16:
+			((u32 *) (info->pseudo_palette))[regno] = v;
+			break;
+		case 24:
+		case 32:
+			((u32 *) (info->pseudo_palette))[regno] = v;
+			break;
+		}
+		return 0;
+	}
+	return 0;
+}
+
+    /*
+     *  Pan or Wrap the Display
+     *
+     *  This call looks only at xoffset, yoffset and the FB_VMODE_YWRAP flag
+     */
+
+static int milkymistfb_pan_display(struct fb_var_screeninfo *var,
+			   struct fb_info *info)
+{
+	if (var->vmode & FB_VMODE_YWRAP) {
+		if (var->yoffset < 0
+		    || var->yoffset >= info->var.yres_virtual
+		    || var->xoffset)
+			return -EINVAL;
+	} else {
+		if (var->xoffset + var->xres > info->var.xres_virtual ||
+		    var->yoffset + var->yres > info->var.yres_virtual)
+			return -EINVAL;
+	}
+	info->var.xoffset = var->xoffset;
+	info->var.yoffset = var->yoffset;
+	if (var->vmode & FB_VMODE_YWRAP)
+		info->var.vmode |= FB_VMODE_YWRAP;
+	else
+		info->var.vmode &= ~FB_VMODE_YWRAP;
+	return 0;
+}
+
+    /*
+     *  Most drivers don't need their own mmap function 
+     */
+
+static int milkymistfb_mmap(struct fb_info *info,
+		    struct vm_area_struct *vma)
+{
+	return -EINVAL;
+}
+
+#ifndef MODULE
+static int __init milkymistfb_setup(char *options)
+{
+	char *this_opt;
+
+	milkymistfb_enable = 1;
+
+	if (!options || !*options)
+		return 1;
+
+	while ((this_opt = strsep(&options, ",")) != NULL) {
+		if (!*this_opt)
+			continue;
+		if (!strncmp(this_opt, "disable", 7))
+			milkymistfb_enable = 0;
+	}
+	return 1;
+}
+#endif  /*  MODULE  */
+
+    /*
+     *  Initialisation
+     */
+
+static int __init milkymistfb_probe(struct platform_device *dev)
+{
+	struct fb_info *info;
+	int retval = -ENOMEM;
+
+	/*
+	 * For real video cards we use ioremap.
+	 */
+	if (!(videomemory = vmalloc(videomemorysize)))
+		return retval;
+
+	/*
+	 * VFB must clear memory to prevent kernel info
+	 * leakage into userspace
+	 * VGA-based drivers MUST NOT clear memory if
+	 * they want to be able to take over vgacon
+	 */
+	memset(videomemory, 0, videomemorysize);
+
+	info = framebuffer_alloc(sizeof(u32) * 256, &dev->dev);
+	if (!info)
+		goto err;
+
+	info->screen_base = (char __iomem *)videomemory;
+	info->fbops = &milkymistfb_ops;
+
+	retval = fb_find_mode(&info->var, info, NULL,
+			      NULL, 0, NULL, 8);
+
+	if (!retval || (retval == 4))
+		info->var = milkymistfb_default;
+	info->fix = milkymistfb_fix;
+	info->pseudo_palette = info->par;
+	info->par = NULL;
+	info->flags = FBINFO_FLAG_DEFAULT;
+
+	retval = fb_alloc_cmap(&info->cmap, 256, 0);
+	if (retval < 0)
+		goto err1;
+
+	retval = register_framebuffer(info);
+	if (retval < 0)
+		goto err2;
+	platform_set_drvdata(dev, info);
+
+	printk(KERN_INFO
+	       "fb%d: Milkymist frame buffer device at %X, using %ldK of video memory\n",
+	       info->node, videomemory, videomemorysize >> 10);
+
+       CSR_VGA_BASEADDRESS = videomemory;
+
+	return 0;
+err2:
+	fb_dealloc_cmap(&info->cmap);
+err1:
+	framebuffer_release(info);
+err:
+	vfree(videomemory);
+	return retval;
+}
+
+static int milkymistfb_remove(struct platform_device *dev)
+{
+	struct fb_info *info = platform_get_drvdata(dev);
+
+	if (info) {
+		unregister_framebuffer(info);
+		vfree(videomemory);
+		framebuffer_release(info);
+	}
+	return 0;
+}
+
+static struct platform_driver milkymistfb_driver = {
+	.probe	= milkymistfb_probe,
+	.remove = milkymistfb_remove,
+	.driver = {
+		.name	= "milkymistfb",
+	},
+};
+
+static struct platform_device *milkymistfb_device;
+
+static int __init milkymistfb_init(void)
+{
+	int ret = 0;
+
+#ifndef MODULE
+	char *option = NULL;
+
+	if (fb_get_options("milkymistfb", &option))
+		return -ENODEV;
+	milkymistfb_setup(option);
 #endif
+
+	if (!milkymistfb_enable)
+		return -ENXIO;
+
+	ret = platform_driver_register(&milkymistfb_driver);
+
+	if (!ret) {
+		milkymistfb_device = platform_device_alloc("milkymistfb", 0);
+
+		if (milkymistfb_device)
+			ret = platform_device_add(milkymistfb_device);
+		else
+			ret = -ENOMEM;
+
+		if (ret) {
+			platform_device_put(milkymistfb_device);
+			platform_driver_unregister(&milkymistfb_driver);
+		}
+	}
+
+	return ret;
+}
+
 module_init(milkymistfb_init);
+
+#ifdef MODULE
+static void __exit milkymistfb_exit(void)
+{
+	platform_device_unregister(milkymistfb_device);
+	platform_driver_unregister(&milkymistfb_driver);
+}
+
 module_exit(milkymistfb_exit);
 
+MODULE_LICENSE("GPL");
+#endif				/* MODULE */
