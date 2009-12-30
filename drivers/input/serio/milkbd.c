47e0c5e4c99ff9d52c7691200dff780f6f863a91
Takeshi MATSUYA <macchan@sfc.wide.ad.jp>
2009-12-30 13:24:12 +0800

    check tx_busy when writing PS/2

--- drivers/input/serio/milkbd.c
+++ drivers/input/serio/milkbd.c
@@ -42,9 +42,13 @@ MODULE_LICENSE("GPL");
  * Register numbers.
  */
 #define	PS2_DATA_REG	0x80007000
+#define	PS2_STATUS_REG	0x80007004
+#define PS2_TX_BUSY	0x01
 
 static int milkbd_write(struct serio *port, unsigned char val)
 {
+	while(readl(PS2_STATUS_REG)&PS2_TX_BUSY);
+
 	writel(val, PS2_DATA_REG);
 
 	return 0;
